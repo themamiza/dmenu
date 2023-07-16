d02d245c51ac2e6308010a5b87414a915d2ff6ed
mamiza <drogoniza@gmail.com>
2023-07-17 00:30:21 +0330

    Center

--- config.def.h
+++ config.def.h
@@ -1,7 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int min_width = 500;                 /* minimum width when centered */
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "Iosevka:size=12", "NotoColorEmoji:size=10" };
